bb2d6934e73b48c9262af405e374bdac14ffa071
Kiana Manning <kmanning718@gmail.com>
2024-11-04 22:49:29 -0500

    semi-finished profile page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,18 +1,64 @@
+"use client";
+import React, { useState } from 'react';
+import '../profile/profile.css';
+import Tag from './tag';
+
 export default function Profile() {
+    // random tags for testing
+    const [sections, setSections] = useState({
+        skills: [
+            {index: 1, text:"HTML"},
+            {index: 2, text:"Java"} , 
+            {index:3, text:"Python"}
+
+        ],
+        interests: [
+            {index:4, text:"Coding"},
+            {index:5, text: "Painting"},
+            {index:6, text:"Gaming"}
+        ],
+    });
+
+    //function to delete tag by filtering index
+    function deleteTag(section, tagId) {
+        setSections((prevSections) => ({
+            ...prevSections,
+            [section]: prevSections[section].filter((tag) => tag.index !== tagId),
+        }));
+
+    };
+
+    // function to add new tags
+    function addTag(section) {
+        const newTagContent = prompt("Write here");
+       if (newTagContent) {
+            const newTag = {
+                index: Date.now(), //create a unique index
+                text: newTagContent,
+            };
+            setSections((prevSections) => ({
+                ...prevSections,
+                [section]: [...prevSections[section], newTag],
+            }))
+        }
+    };
+
+
     return (
         <>
             <h1>Profile</h1>
-            <main className="container mt-5">
-                <div className="row">
+            <main className="profile">
+                <div className="personal-info">
                     {/* Profile Image */}
                     <img
-                        src="images/alice.jpg"
+                        src="./assets/alice.jpg"
                         alt="Profile Image"
-                        className="rounded-circle img-fluid"
-                        style={{ width: 150, height: 150 }}
+                        className="profile-pic"
                     />
+                    {/* Edit button */}
+                    <button className='profile-editbtn'>Edit</button>
                     {/* Profile Details */}
-                    <div className="col-md-8">
+                    <div className="profile-details">
                         <h1>Jane Doe</h1>
                         <p>
                             <strong>Bio:</strong> Lorem ipsum dolor sit amet, consectetur
@@ -23,7 +69,7 @@ export default function Profile() {
                             fugiat nulla pariatur.
                         </p>
                         <h2>Contact Information</h2>
-                        <ul className="list-unstyled">
+                        <ul className="contact info">
                             <li>
                                 <strong>Email:</strong> jane.doe@example.com
                             </li>
@@ -37,60 +83,79 @@ export default function Profile() {
                     </div>
                 </div>
                 <h2>Education</h2>
-                <ul className="educationEx">
-                    <p>University of North Carolina at Charlotte</p>
-                    <p>
-                        <strong>Degree: </strong>Bachelor of Science
-                    </p>
-                    <p>
-                        <strong>Major: </strong>Computer Science
-                    </p>
-                    <p>2021 - 2024</p>
+                <ul className="cards">
+                    <button className='profile-addbtn'>+</button>
+                    <div className='card-info'>
+                        <button className='profile-editbtn'>Edit</button>
+                        <button>Delete</button>
+                        <h3>University of North Carolina at Charlotte</h3>
+                        <p>
+                            <strong>Degree: </strong>Bachelor of Science
+                        </p>
+                        <p>
+                            <strong>Major: </strong>Computer Science
+                        </p>
+                        <p>2021 - 2024</p>
+                    </div>
+
                 </ul>
                 <h2>Certifications</h2>
-                <ul className="certify">
+                <ul className="cards">
+                    <button className='profile-editbtn'>Edit</button>
                     <p>Google Cloud Professional Cloud Architect</p>
                     <p>AWS Certified Solutions Architect Associate</p>
                     <p>Certified Kubernetes Administrator</p>
                 </ul>
                 {/* Additional Details Section */}
-                <section className="mt-5">
-                    <h3>Skills &amp; Interests</h3>
+                <h3>Skills &amp; Interests</h3>
+                <section className="cards">
                     <div className="row">
-                        <div className="col-md-6">
+                        <div className="card-info">
+                            <button className='skills-addbtn'
+                                onClick={() => addTag('skills')}>+</button>
                             <h4>Skills</h4>
-                            <ul>
-                                <li>JavaScript</li>
-                                <li>React</li>
-                                <li>HTML/CSS</li>
-                                <li>Node.js</li>
-                                <li>MongoDB</li>
-                            </ul>
+                            <div className='tag-container'>
+                                {sections.skills.map((tag) => (
+                                    <Tag key={tag.index} text={tag.text}
+                                        onDelete={() => deleteTag('skills', tag.index)} />
+                                ))}
+
+                            </div>
                         </div>
-                        <div className="col-md-6">
+                        <br />
+                        <div className="card-info">
+                            <button className='skills-addbtn'
+                                onClick={() => addTag('interests')}>+</button>
                             <h4>Interests</h4>
-                            <ul>
-                                <li>Coding</li>
-                                <li>Reading</li>
-                                <li>Traveling</li>
-                                <li>Photography</li>
-                            </ul>
+                            <div className='tag-container'>
+                                {sections.interests.map((tag) => (
+                                    <Tag key={tag.index} text={tag.text}
+                                        onDelete={() => deleteTag('interests', tag.index)} />
+                                ))}
+
+                            </div>
                         </div>
                     </div>
                 </section>
-                <section className="experience">
-                    <h2>Experience</h2>
-                    <p>
-                        <strong>Job Title: </strong>Lorem
-                    </p>
-                    <p>
-                        <strong>Company: </strong>Lorem ipsum{" "}
-                    </p>
-                    <p>
-                        <strong>Date of Employment: </strong>October 2021 - Present
-                    </p>
+                <h2>Experience</h2>
+                <section className="cards">
+                    <button className='profile-addbtn'>+</button>
+                    <div className='card-info'>
+                        <button className='profile-editbtn'>Edit</button>
+                        <button>Delete</button>
+                        <p>
+                            <strong>Job Title: </strong>Software Engineer
+                        </p>
+                        <p>
+                            <strong>Company: </strong> Microsoft
+                        </p>
+                        <p>
+                            <strong>Date of Employment: </strong>October 2021 - Present
+                        </p>
+                    </div>
+
                 </section>
             </main>
         </>
     );
-}
+};
