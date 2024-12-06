995f5aaa921e816061ca971b94a2d64b0245a22e
oneofthejosephs <joseph.umoren.12@gmail.com>
2024-12-06 02:30:00 -0500

    Added tests

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -5,6 +5,7 @@ import React, { useState } from "react";
 export default function SignUp() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [repassword, setRepassword] = useState("");
 
     const handleSignUp = (e) => {
         e.preventDefault();
